548f02ebdb7d44630618db27095dd808a1113e6c
Johnny Wang <wangj@garena.com>
2021-07-15 10:43:15 +0800

    UPD: update proxy reactive

--- observer/funcional-version/observer-proxy-effect.js
+++ observer/funcional-version/observer-proxy-effect.js
@@ -2,78 +2,43 @@
 const proxyCache = new WeakMap();
 const rawCache = new WeakMap();
 const trackMap = new WeakMap();
-const effectCache = [];
+const effectBox = [];
 
 function isObject(v) {
   return v !== null && typeof v === 'object';
 }
 
-function reactive(target) {
-  return createReactive(target);
-}
-
-function createReactive(target) {
-  if (!isObject(target)) return target;
-  let observed = proxyCache.get(target);
-  // 拿取之前已產生的代理Proxy
-  if (observed) return observed;
-  // 避免重複代理Proxy
-  if (rawCache.get(target)) return target;
-  const handlers = {
-    get: (target, key, receiver) => {
-      const res = Reflect.get(target, key, receiver);
-      if (res._isRef) return res.value;
-      track(target, key);
-      // 深度自動代理Proxy
-      return reactive(res);
-    },
-    set: (target, key, value, receiver) => {
-      const res = Reflect.set(target,key,value,receiver);
-      trigger(target, key);
-      return res;
-    },
-  }
-  
-  observed = new Proxy(target, handlers);
-  proxyCache.set(target, observed);
-  rawCache.set(observed, target); // 映射表
-  return observed;
-}
-
-// effect 實現
-function effect(fn) {
-  const effect = createEffect(fn);
-  effect();
-  return effect;
-}
-
-function createEffect(fn) {
-  const effect = function() {
-    return runEffect(effect, fn);
-  };
-  return effect;
-}
-
 function runEffect(effect, fn) {
   try {
-    effectCache.push(effect);
+    effectBox.push(effect);
     return fn();
   } finally {
-    effectCache.pop(effect);
+    effectBox.pop(effect);
   }
+}
+
+function watchEffect(fn, options = { lazy: false }) {
+  const effect = () => {
+    return runEffect(effect, fn);
+  };
+  if (!options.lazy) {
+    effect();
+  }
+  effect.options = options;
+  return effect;
 }
 
 // 依賴追蹤
 function track(target, key) {
-  const effect = effectCache[effectCache.length-1];
+  const effect = effectBox[effectBox.length - 1];
   if (effect) {
     let depsMap = trackMap.get(target);
     if (!depsMap) {
-      trackMap.set(target, depsMap = new Map());
+      trackMap.set(target, (depsMap = new Map()));
     }
     let dep = depsMap.get(key);
     if (!dep) {
-      depsMap.set(key, dep = new Set());
+      depsMap.set(key, (dep = new Set()));
     }
     if (!dep.has(effect)) {
       dep.add(effect);
@@ -86,31 +51,90 @@ function trigger(target, key) {
   if (!depsMap) return;
   const effects = depsMap.get(key);
   if (effects) {
-    effects.forEach((effect) => effect());
+    effects.forEach((effect) => {
+      if (effect.options.scheduler) {
+        effect.options.scheduler();
+      } else {
+        effect();
+      }
+    });
   }
 }
 
-// ref 實現
-function ref(raw) {
-  raw = isObject(raw) ? reactive(raw) : raw;
+function defineReactive(target) {
+  if (!isObject(target)) return target;
+  
+  let observed = proxyCache.get(target);
+  // 拿取之前已產生的代理Proxy
+  if (observed) return observed;
+  // 避免重複代理Proxy
+  if (rawCache.get(target)) return target;
+  
+  // proxy
+  observed = new Proxy(target, {
+    get(obj, key, receiver) {
+      track(obj, key);
+      const t = Reflect.get(obj, key, receiver);
+      if (t._isRef) return t.value;
+      // 深度自動代理Proxy
+      return typeof obj[key] === 'object'
+        ? defineReactive(t)
+        : t;
+    },
+    set(obj, key, newVal, receiver) {
+      const t = Reflect.set(obj, key, newVal, receiver);
+      trigger(obj, key);
+      return t;
+    },
+  });
+  
+  proxyCache.set(target, observed);
+  rawCache.set(observed, target); // 映射表
+  
+  return observed;
+}
+
+function ref(value) {
+  let _value = defineReactive(value);
   const v = {
     _isRef: true,
     get value() {
       track(v, '');
-      return raw;
+      return _value;
     },
     set value(newVal) {
-      raw = newVal;
+      _value = newVal;
       trigger(v, '');
-    }
+    },
   };
   return v;
 }
 
+function reactive(value) {
+  return defineReactive(value);
+}
 
-const myName = ref('johnny');
-const d = reactive({
-  a: myName
-})
+function computed(fn) {
+  let cache;
+  let dirty = true;
 
-console.log(d.a);
+  const effect = watchEffect(fn, {
+    lazy: true,
+    scheduler(){
+      if (!dirty) {
+        dirty = true;
+      }
+    },
+  });
+
+  return {
+    _isRef: true,
+    get value() {
+      if (dirty) {
+        cache = effect();
+        dirty = false;
+      }
+      return cache;
+    },
+  };
+}
